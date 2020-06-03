9a0583429de4eee288a6f246983a0bd1f43e77f5
qicaisheng <qicaisheng@qq.com>
2020-06-03 22:25:49 +0800

    extract validateFormatSucceed method

--- src/main/java/com/twschool/practice/GuessNumberGame.java
+++ src/main/java/com/twschool/practice/GuessNumberGame.java
@@ -1,6 +1,8 @@
 package com.twschool.practice;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GuessNumberGame {
     private String[] answer;
@@ -11,6 +13,9 @@ public class GuessNumberGame {
 
     public String guess(String number) {
         String[] answerFromUser = number.split(" ");
+        if (!validateFormatSucceed(answerFromUser)) {
+            return "Wrong Input，Input again";
+        }
         int numberA = 0;
         int numberB = 0;
         for (int index = 0; index < this.answer.length; index ++) {
@@ -22,5 +27,10 @@ public class GuessNumberGame {
         }
         
         return numberA + "A" + numberB + "B";
+    }
+
+    private boolean validateFormatSucceed(String[] answerFromUser) {
+        Set<String> answerSetFromUser = new HashSet<>(Arrays.asList(answerFromUser));
+        return answerSetFromUser.size() == answer.length;
     }
 }
--- src/test/java/com/twschool/practice/GuessNumberGameTest.java
+++ src/test/java/com/twschool/practice/GuessNumberGameTest.java
@@ -41,4 +41,22 @@ public class GuessNumberGameTest {
         Assert.assertEquals("4A0B", result);
     }
 
+    @Test
+    public void should_output_Wrong_input_when_input_1134_given_game_with_answer_1234() {
+        GuessNumberGame guessNumberGame = new GuessNumberGame("1234");
+
+        String result = guessNumberGame.guess("1 1 3 4");
+
+        Assert.assertEquals("Wrong Input，Input again", result);
+    }
+
+    @Test
+    public void should_output_Wrong_input_when_input_12_given_game_with_answer_1234() {
+        GuessNumberGame guessNumberGame = new GuessNumberGame("1234");
+
+        String result = guessNumberGame.guess("1 2");
+
+        Assert.assertEquals("Wrong Input，Input again", result);
+    }
+
 }
