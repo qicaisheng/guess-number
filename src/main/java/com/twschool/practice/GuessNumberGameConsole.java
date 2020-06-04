c210da136b3a62ec0ec03250ea42d6bdacee7baa
qicaisheng <qicaisheng@qq.com>
2020-06-04 17:59:47 +0800

    add construct

--- src/main/java/com/twschool/practice/GuessNumberGameConsole.java
+++ src/main/java/com/twschool/practice/GuessNumberGameConsole.java
@@ -3,15 +3,41 @@ package com.twschool.practice;
 import java.util.stream.Collectors;
 
 public class GuessNumberGameConsole {
+    private static final String WELCOME_MESSAGE = "Welcome to Guess Number!\nGame Start!\n";
     private static final String WRONG_INPUT = "Wrong Input，Input again";
+    private static final String WINNING_MESSAGE = "Congratulation! You win!";
+    private static final String INPUT_TIPS = "Please guess:";
     private static final String NEW_LINE = "\n";
+    private static final String LOSE_MESSAGE = "You failed!";
+    public static final String GUESS_RESULT_FORMAT = "Result: %s";
+    public static final String GUESS_HISTORIES_FORMAT = "Histories: %s";
     private GuessNumberGame guessNumberGame;
-    
-    public static void main(String[] args) {
+    private ConsoleInputReader consoleInputReader;
+
+    public GuessNumberGameConsole() {
+        this.guessNumberGame = new GuessNumberGame(new AnswerGeneratorImpl());
+        this.consoleInputReader = new ConsoleInputReader();
     }
 
     public GuessNumberGameConsole(GuessNumberGame guessNumberGame) {
         this.guessNumberGame = guessNumberGame;
+        this.consoleInputReader = new ConsoleInputReader();
+    }
+
+    public void start() {
+        System.out.println(WELCOME_MESSAGE);
+
+        while (this.guessNumberGame.getStatus() == GameStatus.PENDING) {
+            System.out.println(INPUT_TIPS);
+            String guessResult = guess(consoleInputReader.getInput());
+            System.out.println(String.format(GUESS_RESULT_FORMAT, guessResult));
+            System.out.println(String.format(GUESS_HISTORIES_FORMAT, getHistories()));
+        }
+        if (this.guessNumberGame.getStatus() == GameStatus.SUCCEED) {
+            System.out.println(WINNING_MESSAGE);
+        } else if (this.guessNumberGame.getStatus() == GameStatus.SUCCEED) {
+            System.out.println(LOSE_MESSAGE);
+        }
     }
 
     public String guess(String number) {
---
+++ src/main/java/com/twschool/practice/GuessNumberGameConsoleApp.java
@@ -0,0 +1,9 @@
+package com.twschool.practice;
+
+public class GuessNumberGameConsoleApp {
+
+    public static void main(String[] args) {
+        GuessNumberGameConsole guessNumberGameConsole = new GuessNumberGameConsole();
+        guessNumberGameConsole.start();
+    }
+}
