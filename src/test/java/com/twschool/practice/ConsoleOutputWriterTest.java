bfe195ae08fb54fc04bd93a5dc98b9a927d5dc75
qicaisheng <qicaisheng@qq.com>
2020-06-05 15:39:12 +0800

    add ConsoleOutputWriter class

---
+++ src/test/java/com/twschool/practice/ConsoleOutputWriterTest.java
@@ -0,0 +1,22 @@
+package com.twschool.practice;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ConsoleOutputWriterTest {
+    @Test
+    public void should_output_to_console_when_output() {
+        ConsoleOutputWriter writer = new ConsoleOutputWriter();
+        PrintStream out = mock(PrintStream.class);
+        System.setOut(out);
+
+        writer.output("Welcome to Guess Number!\nGame Start!");
+
+        verify(out).println(Mockito.eq("Welcome to Guess Number!\nGame Start!"));
+    }
+}
