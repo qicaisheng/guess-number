7fa8a8376156ec49b45c8b603e1e5dd05ccf87bd
qicaisheng <qicaisheng@qq.com>
2020-06-05 15:43:20 +0800

    extract interface

--- src/main/java/com/twschool/practice/console/ConsoleOutputWriter.java
+++ src/main/java/com/twschool/practice/console/ConsoleOutputWriter.java
@@ -1,6 +1,8 @@
 package com.twschool.practice.console;
 
-public class ConsoleOutputWriter {
+public class ConsoleOutputWriter implements OutputWriter {
+    
+    @Override
     public void output(String message) {
         System.out.println(message);
     }
---
+++ src/main/java/com/twschool/practice/console/OutputWriter.java
@@ -0,0 +1,5 @@
+package com.twschool.practice.console;
+
+public interface OutputWriter {
+    void output(String message);
+}
