18fb8004c798acfdef15af8a4e4cc643f18739fd
qicaisheng <qicaisheng@qq.com>
2020-06-05 14:50:49 +0800

    small refactor

--- src/main/java/com/twschool/practice/console/ConsoleInputReader.java
+++ src/main/java/com/twschool/practice/console/ConsoleInputReader.java
@@ -3,6 +3,8 @@ package com.twschool.practice.console;
 import java.util.Scanner;
 
 public class ConsoleInputReader implements InputReader {
+    
+    @Override
     public String getInput() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
--- src/main/java/com/twschool/practice/domain/RandomAnswerGenerator.java
+++ src/main/java/com/twschool/practice/domain/RandomAnswerGenerator.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RandomAnswerGenerator implements AnswerGenerator {
-    public RandomAnswerGenerator() {
-    }
 
     @Override
     public Answer generateAnswer() {
