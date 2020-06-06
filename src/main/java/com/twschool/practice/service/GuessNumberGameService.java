491c9b35acff79443c1ca66923bb9bcb67ed43be
qicaisheng <qicaisheng@qq.com>
2020-06-06 15:37:15 +0800

    should_return_save_game_record_when_guess_number

--- src/main/java/com/twschool/practice/service/GuessNumberGameService.java
+++ src/main/java/com/twschool/practice/service/GuessNumberGameService.java
@@ -1,15 +1,30 @@
 package com.twschool.practice.service;
 
+import com.twschool.practice.GameRecord;
+import com.twschool.practice.GameRecordMapper;
 import com.twschool.practice.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class GuessNumberGameService {
+
+    @Autowired
+    private GameRecordMapper gameRecordMapper;
+    private static final String ROUND = "round1";
+
+    public GuessNumberGameService(GameRecordMapper gameRecordMapper) {
+        this.gameRecordMapper = gameRecordMapper;
+    }
+    
     
     public CompareResult guess(Answer answer) {
-        return getGuessNumberGame().guess(answer);
+        CompareResult guess = getGuessNumberGame().guess(answer);
+        GuessHistory guessHistory = new GuessHistory(answer, guess);
+        gameRecordMapper.save(new GameRecord(guessHistory, ROUND));
+        return guess;
     }
 
     public List<GuessHistory> getGuessHistories() {
--- src/test/java/com/twschool/practice/service/GuessNumberGameServiceTest.java
+++ src/test/java/com/twschool/practice/service/GuessNumberGameServiceTest.java
@@ -1,23 +1,31 @@
 package com.twschool.practice.service;
 
+import com.twschool.practice.GameRecord;
+import com.twschool.practice.GameRecordMapper;
 import com.twschool.practice.domain.Answer;
 import com.twschool.practice.domain.CompareResult;
 import com.twschool.practice.domain.GuessHistory;
 import com.twschool.practice.domain.GuessNumberGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class GuessNumberGameServiceTest {
     
     private GuessNumberGameService guessNumberGameService;
     private GuessNumberGame guessNumberGame;
     
+    private GameRecordMapper mockGameRecordMapper;
+    
     @BeforeEach
     void setUp() {
-        guessNumberGameService = Mockito.spy(new GuessNumberGameService());
+        mockGameRecordMapper = Mockito.mock(GameRecordMapper.class);
+        guessNumberGameService = Mockito.spy(new GuessNumberGameService(mockGameRecordMapper));
         guessNumberGame = Mockito.mock(GuessNumberGame.class);
         Mockito.when(guessNumberGame.guess(Mockito.any())).thenReturn(new CompareResult(1, 2));
         Mockito.when(guessNumberGame.getGuessHistories()).thenReturn(Arrays.asList(new GuessHistory(new Answer("1 4 5 8"), new CompareResult(1, 3))));
@@ -31,6 +39,26 @@ class GuessNumberGameServiceTest {
         
         Mockito.verify(guessNumberGame, Mockito.times(1)).guess(Mockito.any());
         
+    }
+
+    @Test
+    void should_return_save_game_record_when_guess_number() {
+        guessNumberGameService.guess(new Answer("1 2 3 4"));
+
+        ArgumentCaptor<GameRecord> argumentCaptor = ArgumentCaptor.forClass(GameRecord.class);
+
+        Mockito.verify(mockGameRecordMapper, Mockito.times(1)).save(argumentCaptor.capture());
+
+        GameRecord gameRecord = argumentCaptor.getValue();
+        assertAll(("game record asserts"),
+                () -> assertNotNull(gameRecord),
+                () -> assertEquals("round1", gameRecord.getRound()),
+                () -> assertEquals("1 2 3 4", gameRecord.getUserGuess()),
+                () -> assertEquals(1, gameRecord.getPositionCorrectNumber()),
+                () -> assertEquals(2, gameRecord.getValueCorrectNumber()),
+                () -> assertNotNull(gameRecord)
+        );
+
     }
 
     @Test
