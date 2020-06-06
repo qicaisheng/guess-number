69f1a96dc940a096f13cf87a98714e428ad1cd7e
qicaisheng <qicaisheng@qq.com>
2020-06-06 10:41:22 +0800

    use spring mock mvc

--- src/test/java/com/twschool/practice/api/GuessNumberGameControllerTest.java
+++ src/test/java/com/twschool/practice/api/GuessNumberGameControllerTest.java
@@ -1,76 +1,67 @@
 package com.twschool.practice.api;
 
+import com.twschool.practice.domain.Answer;
 import com.twschool.practice.domain.CompareResult;
+import com.twschool.practice.domain.GuessHistory;
 import com.twschool.practice.service.GuessNumberGameService;
-import io.restassured.http.ContentType;
-import io.restassured.mapper.TypeRef;
-import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import javax.annotation.PostConstruct;
-import java.lang.reflect.Type;
-import java.util.List;
+import java.util.Arrays;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class GuessNumberGameControllerTest {
 
     @MockBean
     private GuessNumberGameService guessNumberGameService;
-
-    private String uri;
     
-    @LocalServerPort
-    private int port;
+    @Autowired
+    private MockMvc mockMvc;
     
-    @PostConstruct
-    public void init() {
-        uri = "http://localhost:" + port;
-    }
     @BeforeEach
     void setUp() {
-        Mockito.when(guessNumberGameService.guess(Mockito.any())).thenReturn(new CompareResult(1, 2));
+        CompareResult compareResult = new CompareResult(1, 2);
+        Answer answer = new Answer(Arrays.asList("1", "2", "3", "4"));
+        Mockito.when(guessNumberGameService.guess(Mockito.any())).thenReturn(compareResult);
+        Mockito.when(guessNumberGameService.getGuessHistories()).thenReturn(Arrays.asList(new GuessHistory(answer, compareResult)));
     }
 
     @Test
-    void should_return_result_when_guess_number() {
-
-        given()
-                .contentType(ContentType.JSON)
-                .body(new AnswerDto("1 2 3 4"))
-                .when()
-                .post(uri + "/guess-number/compare-results")
-        .then()
-                .assertThat()
-                .body("valueAndPositionCorrectNumber", equalTo(1))
-                .body("valueCorrectButPositionIncorrectNumber", equalTo(2));
+    void should_return_result_when_guess_number() throws Exception {
         
+        this.mockMvc.perform(post("/guess-number/compare-results")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"number\":\"1 2 3 4\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.valueAndPositionCorrectNumber").value(1))
+                .andExpect(jsonPath("$.valueCorrectButPositionIncorrectNumber").value(2));
     }
 
     @Test
-    void should_return_histories_when_get_histories() {
-
-        Response response = given()
-                .contentType(ContentType.JSON)
-                .when()
-                .get(uri + "/guess-number/histories");
-
-        List<GuessHistoryDto> guessHistories = response.getBody().as(new TypeRef<List<GuessHistoryDto>>() {
-            @Override
-            public Type getType() {
-                return super.getType();
-            }
-        });
+    void should_return_histories_when_get_histories() throws Exception {
         
-        assertNotNull(guessHistories);
+        this.mockMvc.perform(get("/guess-number/histories")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].answerDto", Matchers.notNullValue()))
+                .andExpect(jsonPath("$[0].answerDto.number").value("1234"))
+                .andExpect(jsonPath("$[0].compareResultDto.valueAndPositionCorrectNumber").value(1))
+                .andExpect(jsonPath("$[0].compareResultDto.valueCorrectButPositionIncorrectNumber").value(2));
     }
 
 
